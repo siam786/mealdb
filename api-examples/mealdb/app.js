9ff27d854f2c04129d806243533f159af543dc9d
siam786 <shohrab.hossain36@gmail.com>
2021-08-29 08:10:38 +0600

    search result fix

--- api-examples/mealdb/app.js
+++ api-examples/mealdb/app.js
@@ -3,7 +3,7 @@
 const searchFood = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  searchText.value = ''
+  searchField.value = ''
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
   fetch(url)
     .then((res) => res.json())
@@ -12,6 +12,7 @@ const searchFood = () => {
 
 const updateFoodResult = (meals) => {
   const searchFood = document.getElementById("searchresult");
+  searchFood.innerHTML = ''
   //console.log(meals);
   meals.forEach((meal) => {
     //console.log(meal);
